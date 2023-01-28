efe043b478ae45d34c88cf32d865fa400a7a07a4
felipemaxplay <felipemaxplay@gmail.com>
2023-01-28 15:42:07 -0300

    domain completed

--- src/br/com/felipemaxplay/challenge/domain/Dev.java
+++ src/br/com/felipemaxplay/challenge/domain/Dev.java
@@ -2,22 +2,36 @@ package br.com.felipemaxplay.challenge.domain;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
     private String name;
-    private Set<Content> ContentsSubscribes = new LinkedHashSet<>();
-    private Set<Content> ContentsFinished = new LinkedHashSet<>();
+    private Set<Content> contentsSubscribes = new LinkedHashSet<>();
+    private Set<Content> contentsFinished = new LinkedHashSet<>();
 
     @Deprecated
     public Dev() {
     }
 
-    public void SubscribeBootCamp(Bootcamp bootcamp) {};
+    public void SubscribeBootCamp(Bootcamp bootcamp) {
+        this.getContentsSubscribes().addAll(bootcamp.getContents());
+        bootcamp.getDevs().add(this);
+    };
 
-    public void progression() {};
+    public void progression() {
+        Optional<Content> content = this.contentsSubscribes.stream().findFirst();
+        if(content.isPresent()) {
+            this.contentsFinished.add(content.get());
+            this.contentsSubscribes.remove(content.get());
+        } else {
+            System.err.println("You are not subscribed to any content.");
+        }
+    };
 
-    public void calculateTotalXp() {};
+    public double calculateTotalXp() {
+        return this.contentsFinished.stream().mapToDouble(Content::calculateXp).sum();
+    };
 
     public String getName() {
         return name;
@@ -28,19 +42,19 @@ public class Dev {
     }
 
     public Set<Content> getContentsSubscribes() {
-        return ContentsSubscribes;
+        return contentsSubscribes;
     }
 
     public void setContentsSubscribes(Set<Content> contentsSubscribes) {
-        ContentsSubscribes = contentsSubscribes;
+        contentsSubscribes = contentsSubscribes;
     }
 
     public Set<Content> getContentsFinished() {
-        return ContentsFinished;
+        return contentsFinished;
     }
 
     public void setContentsFinished(Set<Content> contentsFinished) {
-        ContentsFinished = contentsFinished;
+        contentsFinished = contentsFinished;
     }
 
     @Override
@@ -48,11 +62,11 @@ public class Dev {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Dev dev = (Dev) o;
-        return Objects.equals(name, dev.name) && Objects.equals(ContentsSubscribes, dev.ContentsSubscribes) && Objects.equals(ContentsFinished, dev.ContentsFinished);
+        return Objects.equals(name, dev.name) && Objects.equals(contentsSubscribes, dev.contentsSubscribes) && Objects.equals(contentsFinished, dev.contentsFinished);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, ContentsSubscribes, ContentsFinished);
+        return Objects.hash(name, contentsSubscribes, contentsFinished);
     }
 }
